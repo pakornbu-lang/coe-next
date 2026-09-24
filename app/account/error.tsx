cb38fd3c266532c783d49267060c9288932d0610
Pakorn Bunkongmak <boonkoingmak@gmail.com>
2026-09-24 23:32:38 +0700

    หน้าโหลดแบบ Skeleton

--- app/account/error.tsx
+++ app/account/error.tsx
@@ -1,4 +1,2 @@
 "use client";
-export default function AccountError({ reset }: { reset: () => void }) {
-  return <section className="panel"><h1>โหลดโปรไฟล์ไม่สำเร็จ</h1><p>กรุณาลองใหม่อีกครั้ง</p><button onClick={reset}>ลองใหม่</button></section>;
-}
+export { default } from "@/app/error";
